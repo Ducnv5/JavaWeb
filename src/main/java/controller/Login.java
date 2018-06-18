76bd88b0c643af4663faa94d8eb8affc544e89d6
duc.nguyenvan3 <duc.nguyenvan3@hanwks0059.han.gameloft.org>
2018-06-18 17:23:48 +0700

    +add spring-security

---
+++ src/main/java/controller/Login.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.nio.charset.StandardCharsets;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class Login {
+	
+	@RequestMapping(value = { "/login", "/Login" })
+	public String login(@RequestParam(value = "error", required = false) final String error, final Model model) {
+		if (error != null) {
+			model.addAttribute("message", "Login Failed!");
+		}
+		return "login";
+	}
+
+	@RequestMapping("/admin")
+	public String admin() {
+		return "admin";
+	}
+
+	@RequestMapping("/logout")
+	public String logout(final Model model) {
+		model.addAttribute("message", "Logged out!");
+		return "login";
+	}
+
+}
