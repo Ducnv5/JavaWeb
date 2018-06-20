28d663436b7e456e946ecbe65ec7757067f81eab
duc.nguyenvan3 <duc.nguyenvan3@hanwks0059.han.gameloft.org>
2018-06-20 16:58:19 +0700

    *edit the article entity

--- src/main/java/entity/Article.java
+++ src/main/java/entity/Article.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.Date;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 
@@ -12,14 +14,26 @@ public class Article {
 	@NotNull(message = "Id may not be null")
 	private String header;
 	
-	@NotNull(message = "Id may not be null")
+	@NotNull(message = "body may not be null")
 	private String body;
 	
-	@NotNull(message = "Id may not be null")
+	@NotNull(message = "preview may not be null")
+	private String preview;
+	
+	@NotNull(message = "segment may not be null")
 	private String segment;
 	
-	@NotNull(message = "Id may not be null")
+	@NotNull(message = "group may not be null")
 	private String group;
+	
+	@NotNull(message = "link may not be null")
+	private String href_1;
+	
+	@NotNull(message = "link may not be null")
+	private String href_2;
+	
+	@NotNull(message = "group may not be null")
+	private Date input_time;
 	
 	private int id;
 	
@@ -37,6 +51,54 @@ public class Article {
 		return header;
 	}
 	
+	public String getPreview() {
+		return preview;
+	}
+
+	public void setPreview(String preview) {
+		this.preview = preview;
+	}
+
+	public String getSegment() {
+		return segment;
+	}
+
+	public void setSegment(String segment) {
+		this.segment = segment;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public String getHref_1() {
+		return href_1;
+	}
+
+	public void setHref_1(String href_1) {
+		this.href_1 = href_1;
+	}
+
+	public String getHref_2() {
+		return href_2;
+	}
+
+	public void setHref_2(String href_2) {
+		this.href_2 = href_2;
+	}
+
+	public Date getInput_time() {
+		return input_time;
+	}
+
+	public void setInput_time(Date input_time) {
+		this.input_time = input_time;
+	}
+
 	public String getBody()
 	{
 		return header;
